fb8e26e0c45a21b63912e1b6bbde7c1b7ef55b64
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-12 11:09:04 +0530

    RemotePost helper class implemented
    Common helper class for remote post with API call is developed

--- PaymentGateways/Controllers/PayUMoneyController.cs
+++ PaymentGateways/Controllers/PayUMoneyController.cs
@@ -1,6 +1,8 @@
-﻿using PaymentGateways.Models;
+﻿using PaymentGateways.Helpers;
+using PaymentGateways.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -27,6 +29,45 @@ namespace PaymentGateways.Controllers
                 payuRequest.state = "Gujarat";
                 payuRequest.zipcode = "392012";
                 return View(payuRequest);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public void PayUMoney(PayuRequest data)
+        {
+            try
+            {
+                RemotePost myremotepost = new RemotePost();
+                string key = ConfigurationManager.AppSettings["MERCHANT_KEY"];
+                string salt = ConfigurationManager.AppSettings["SALT"];
+                string surl = ConfigurationManager.AppSettings["SUCC_URL"];
+                string furl = ConfigurationManager.AppSettings["FAIL_URL"];
+
+                //posting all the parameters required for integration.
+                myremotepost.Url = ConfigurationManager.AppSettings["PAYU_BASE_URL"];
+                myremotepost.Add("key", key);
+                string txnid = Guid.NewGuid().ToString().Substring(12);
+                myremotepost.Add("txnid", txnid);
+                myremotepost.Add("amount", data.amount);
+                myremotepost.Add("productinfo", data.productInfo);
+                myremotepost.Add("firstName", data.firstName);
+                myremotepost.Add("lastname", data.lastname);
+                myremotepost.Add("email", data.email);
+                myremotepost.Add("phone", data.phone);
+                myremotepost.Add("address1", data.address1);
+                myremotepost.Add("address2", data.address2);
+                myremotepost.Add("city", data.city);
+                myremotepost.Add("state", data.state);
+                myremotepost.Add("zipcode", data.zipcode);
+                myremotepost.Add("surl", surl);
+                myremotepost.Add("furl", furl);
+                string hashString = key + "|" + txnid + "|" + data.amount + "|" + data.productInfo + "|" + data.firstName + "|" + data.email + "|||||||||||" + salt;
+                myremotepost.Post();
             }
             catch
             {
---
+++ PaymentGateways/Helpers/RemotePost.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PaymentGateways.Helpers
+{
+    public class RemotePost
+    {
+        private System.Collections.Specialized.NameValueCollection Inputs = new System.Collections.Specialized.NameValueCollection();
+
+        public string Url = "";
+        public string Method = "post";
+        public string FormName = "form1";
+
+        public void Add(string name, string value)
+        {
+            Inputs.Add(name, value);
+        }
+
+        public void Post()
+        {
+            HttpContext.Current.Response.Clear();
+
+            HttpContext.Current.Response.Write("<html><head>");
+
+            HttpContext.Current.Response.Write(string.Format("</head><body onload=\"document.{0}.submit()\">", FormName));
+            HttpContext.Current.Response.Write(string.Format("<form name=\"{0}\" method=\"{1}\" action=\"{2}\" >", FormName, Method, Url));
+            for (int i = 0; i < Inputs.Keys.Count; i++)
+            {
+                HttpContext.Current.Response.Write(string.Format("<input name=\"{0}\" type=\"hidden\" value=\"{1}\">", Inputs.Keys[i], Inputs[Inputs.Keys[i]]));
+            }
+            HttpContext.Current.Response.Write("</form>");
+            HttpContext.Current.Response.Write("</body></html>");
+
+            HttpContext.Current.Response.End();
+        }
+    }
+}

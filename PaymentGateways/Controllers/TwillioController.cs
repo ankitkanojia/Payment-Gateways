7cb2becc31ea5aa37cdd86340e7fbc1b9589221b
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-13 10:47:19 +0530

    Twillio API integration
    Twillio API for SMS  send from one mobile to another is completed

---
+++ PaymentGateways/Controllers/TwillioController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using Twilio;
+
+namespace PaymentGateways.Controllers
+{
+    public class TwillioController : Controller
+    {
+        public ActionResult TwillioSMS()
+        {
+            /*Need to install below library
+              Install-Package Twilio
+            */
+
+            try
+            {
+                // Find your Account Sid and Auth Token at twilio.com/user/account
+                string AccountSid = WebConfigurationManager.AppSettings["Twillio_AccountSid"].ToString();
+                string AuthToken = WebConfigurationManager.AppSettings["Twillio_AuthToken"].ToString();
+
+                var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+                //Send SMS
+                var message = twilio.SendMessage("+919099673090", "+919879322283", "This is test SMS");
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+            return View();
+        }
+    }
+}

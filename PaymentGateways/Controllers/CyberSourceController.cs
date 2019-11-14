aa8c88ead4bf5cce798880480b49c40d030dba20
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2019-11-14 15:09:32 +0530

    CyberSource controller integrated
    CyberSource controller and action method integrated and redirection to CyberSource from link is developed on home page

---
+++ PaymentGateways/Controllers/CyberSourceController.cs
@@ -0,0 +1,22 @@
+﻿using PaymentGateways.Models;
+using System;
+using System.Web.Mvc;
+
+namespace PaymentGateways.Controllers
+{
+    public class CyberSourceController : Controller
+    {
+        public ActionResult Index(CyberSourceVM data)
+        {
+            /*Note: -- THIS IS MOST IMPORTANT BEFORE START CODE
+          You need to setup setting in your CYBER SOURCE account else nothing works.
+          You need to specufy return URL in your CYBER SOURCE account
+          You need to check all parameter in DICTIONARY object and CLASS object is same in "DoCyberSorcePayment" method else you got unauthorise error
+          */
+
+            data.TrasactionID = Guid.NewGuid().ToString().Substring(10);
+
+            return RedirectToAction("DoCyberSorcePayment", "PaymentGateway", data);
+        }
+    }
+}
